d6aa0dc1aacf67b6294b3c857048aa27ab28137a
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-03-23 16:20:47 +0100

    add plain data + dockerfile

---
+++ script.js
@@ -0,0 +1,66 @@
+const money = [
+  {
+    title: "US Dollar",
+    sub: "$",
+    text: "USD",
+  },
+  {
+    title: "Euro",
+    sub: "€",
+    text: "EUR",
+  },
+  {
+    title: "Chinese Yuan",
+    sub: "¥",
+    text: "CNY",
+  },
+  {
+    title: "Thai Baht",
+    sub: "฿",
+    text: "THB",
+  },
+  {
+    title: "British Pound Sterling",
+    sub: "£",
+    text: "GBP",
+  },
+  {
+    title: "Ghanaian Cedi",
+    sub: "₵",
+    text: "GHS",
+  },
+  {
+    title: "Japanese Yen",
+    sub: "￥",
+    text: "JPY",
+  },
+  {
+    title: "Polish Zloty",
+    sub: "zł",
+    text: "PLN",
+  },
+];
+let beerHTML = "";
+
+money.map((money, i) => {
+  beerHTML += `
+    <div class="money-card">
+      <h3 class="money-title">${money.title}</h3>
+      <p class="money-sub">${money.sub}</p>
+      <div class="money-line"></div>
+      <p class="money-text">${money.text}</p>
+      <button class="money-button">
+      Save
+    </button>
+    </div>
+  `;
+});
+
+function loadEvent() {
+  console.log("page has been loaded");
+  document
+    .querySelector(".money-card-container")
+    .insertAdjacentHTML("beforeend", beerHTML);
+}
+
+window.addEventListener("load", loadEvent);
